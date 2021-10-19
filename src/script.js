fbfc8db68e6a2abbfb74e284046e6055870bb34a
pedro.jardim <pedro.d.jardim@hotmail.com>
2021-10-18 23:20:03 -0300

    #37 - Contatos na area de footer

---
+++ src/script.js
@@ -0,0 +1,10 @@
+var prods = '';
+for (i = 0; i < db.dados.length; i++) {
+    prods += `<p class="produto-item"> ${db.dados[i].titulo} <br> <img src="${db.dados[i].imagem}"> <br> ${db.dados[i].descricao} <br> R$: ${db.dados[i].preco} <br> </p>`;
+}
+document.getElementById('lista-produtos').innerHTML = prods;
+
+
+function getLink(link){
+    window.location.href = link;
+}
